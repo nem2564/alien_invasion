b4884a7e61c9cadff565c121aa3f66452e355ca7
nem2564 <nem2564@gmail.com>
2023-06-16 16:53:10 -0700

    Preparing the ship
    - Adding the Ship Image
    - Creating the Ship Class
    - Drawing the Ship to the Screen
    
    - Adding gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -20,6 +21,8 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -30,6 +33,7 @@ class AlienInvasion:
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screeen visible.
             pygame.display.flip()
